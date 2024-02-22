b00c5ffc610bf3c6646f9004d508ab76c1a69461
filip k <filip2006kujawa@gmail.com>
2024-02-22 21:53:02 +0100

    podejsc sie spytac

--- src/controlers.py
+++ src/controlers.py
@@ -9,8 +9,15 @@ class UserControlers:
     def get_all_aviable_users(self):
         return jsonify(UserControlers.temp_user_storage)
 
+    def get_single_user(id):
+        if id in UserControlers.temp_user_storage:
+            return jsonify(UserControlers.temp_user_storage.id)
+        else:
+            return {f"User not found with following id : {id}"} , 404
+
+
     def create_an_user(self):
-        new_temp_user= {
+        new_temp_user = {
             "id": len(UserControlers.temp_user_storage) + 1,
             "firstName": str,
             "lastName": str,
@@ -18,4 +25,11 @@ class UserControlers:
             "group": str
         }
         UserControlers.temp_user_storage.append(new_temp_user)
-        return jsonify(new_temp_user),201
+        return jsonify(new_temp_user), 201
+
+    def delete_user(id):
+        if id in UserControlers.temp_user_storage:
+            UserControlers.temp_user_storage.remove(user)
+            return jsonify(user)
+        else:
+            return jsonify({f"Following id : {id} not found"}), 404
