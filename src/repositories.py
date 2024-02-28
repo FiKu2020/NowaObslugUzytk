50c3eda4f7311ed4fed41e3dfff556dc5d5dbf22
filip k <filip2006kujawa@gmail.com>
2024-02-28 13:04:13 +0100

    ok

--- src/repositories.py
+++ src/repositories.py
@@ -7,11 +7,11 @@ class UserControlers:
 
 
     def get_all_aviable_users(self):
-        return jsonify(UserControlers.temp_user_storage)
+        return UserControlers.temp_user_storage
 
     def get_single_user(id):
         if id in UserControlers.temp_user_storage:
-            return jsonify(UserControlers.temp_user_storage.id)
+            return UserControlers.temp_user_storage.id
         else:
             return {f"User not found with following id : {id}"} , 404
 
@@ -25,13 +25,13 @@ class UserControlers:
             "group": str
         }
         UserControlers.temp_user_storage.append(new_temp_user)
-        return jsonify(new_temp_user), 201
+        return new_temp_user, 201
 
     def delete_user(self, id):
         if id in UserControlers.temp_user_storage:
             UserControlers.temp_user_storage.remove(User)
-            return jsonify(User)
+            return User
         else:
-            return jsonify({f"Following id : {id} not found"}), 404
+            return {f"Following id : {id} not found"}, 404
     def update_user(self):
         pass
